9f574afb7b907c7409b7b21b4825d1ce83f5de57
Raheem2003shaik <raheemshaik2003@gmail.com>
2024-10-26 16:29:35 +0530

    message after int

--- Hello.java
+++ Hello.java
@@ -8,5 +8,6 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("hey");
+        System.out.println("after integer initialized");
     }
 }
