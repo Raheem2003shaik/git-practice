42662c6f539e09c802b56b5a1bcda05e39dda737
Raheem2003shaik <raheemshaik2003@gmail.com>
2024-10-26 16:05:56 +0530

    4

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 class Hello {
+
+    public static int add(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("hey");
